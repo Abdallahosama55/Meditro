1e2507019e64454d7427dc7ed4e36b5bed3a32c9
Abdallah Osama <82631323+Abdallahosama55@users.noreply.github.com>
2023-09-21 04:28:42 +0300

    upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+
+import './App.css';
+import Home from './Pages/Home/Home';
+import ContactUs from './Pages/ContactUs/ContactUs';
+import Services from './Pages/Services/Services';
+import Blog from './Pages/Blog/Blog';
+import AboutUs from './Pages/AboutUs/AboutUs';
+import Login from './Pages/Login/Login';
+import SignUp from './Pages/SignUp/SignUp';
+import ErrorPage from './Pages/errorPage/errorPage';
+
+function App() {
+  return (
+    <>
+      <Router>
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/contactus' element={<ContactUs />}/>
+            <Route path='/services' element={<Services />}/>
+            <Route path='/blog' element={<Blog />}/>
+            <Route path='/aboutus' element={<AboutUs />}/>
+            <Route path='/login' element={<Login />}/>
+            <Route path='/signup' element={<SignUp />}/>
+            <Route path='*' element={<ErrorPage/>}/>
+          </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/errorPage/errorPage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Navbar from '../../Components/Nav/Navbar'
+import Footer from '../../Components/Footer/Footer'
+
+const ErrorPage= () => {
+    return (
+      <>
+      <Navbar/>
+      <h1 className='text-center mt-5 text-bold'>PAGE NOT FOUND</h1>
+     <Footer/>
+      </>
+    )
+  }
+  
+  export default ErrorPage
+
+
+  
